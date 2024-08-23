705174e88a24ec4beb01e15b87655c235beb991a
betocostadev <robertomarx@gmail.com>
2024-08-23 16:33:51 -0300

    Feat: Added Dashboard cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import DashboardCard from '@/components/Dashboard/DashboardCard'
-import { Newspaper } from 'lucide-react'
+import { Newspaper, Folders, User, MessageCircle } from 'lucide-react'
 
 export default function Home() {
   return (
@@ -8,7 +8,22 @@ export default function Home() {
         <DashboardCard
           title="Posts"
           count={100}
-          icon={<Newspaper className="text-slate-500" size={48} />}
+          icon={<Newspaper className="text-slate-500" size={40} />}
+        />
+        <DashboardCard
+          title="Categories"
+          count={10}
+          icon={<Folders className="text-slate-500" size={40} />}
+        />
+        <DashboardCard
+          title="Users"
+          count={580}
+          icon={<User className="text-slate-500" size={40} />}
+        />
+        <DashboardCard
+          title="Comments"
+          count={1207}
+          icon={<MessageCircle className="text-slate-500" size={40} />}
         />
       </div>
     </>
